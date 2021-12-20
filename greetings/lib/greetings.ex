b639574c0c0c6d931a860417845b9ed54291ad9f
Nicholas Hammond <hammondn649@gmail.com>
2021-12-21 10:45:16 +1100

    make changes to attendance

--- greetings/lib/greetings.ex
+++ greetings/lib/greetings.ex
@@ -3,7 +3,7 @@ defmodule Greetings do
   nameArray = ["Mike", "Kristina", "Justin", "Josh", "Olivia", "Rebecca", "Nick", "Jeff", "Kathryn", "Paul", "Rob", "Clem", "Will", "Jeremy", "Mark", "Thomas"]
 
   def teamMembers do
-    ["Mike", "Kristina"]
+    ["Bob", "Alice"]
   end
 
   def stringEqualilty(string1, string2) do
